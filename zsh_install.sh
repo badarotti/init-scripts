9c642ee154a91c85729df95c9977fd9b7f4b580f
Ahmed Badr <ab@111virtual.com>
2023-11-15 22:33:46 +0200

    Initial scripts upload. Not tested.

---
+++ zsh_install.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+function error {
+  echo -e "\\e[91m$1\\e[39m"
+  exit 1
+}
+
+function check_internet() {
+  printf "Checking if you are online..."
+  wget -q --spider http://github.com
+  if [ $? -eq 0 ]; then
+    echo "Online. Continuing."
+  else
+    error "Offline. Go connect to the internet then run the script again."
+  fi
+}
+
+check_internet
+
+sudo apt install zsh
+
+chch -s /usr/bin/zsh
+
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+
+sed -i 's/robbyrussel/agnoster/' ~/.zshrc
+
+echo "Remember to logoff/reboot for the changes to take effect."
